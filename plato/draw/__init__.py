8b133dccc16b460247603afe5da33f0f6e7e6593
Matthew Spellings <mspells@umich.edu>
2018-03-20 13:48:37 -0400

    Fixed setup.py for real installation

---
+++ plato/draw/__init__.py
@@ -0,0 +1,2 @@
+# This is needed to import subpackages, like plato.draw.matplotlib
+pass
--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(name='plato',
       description='Geometry and visualization tools',
       author='Matthew Spellings',
       author_email='mspells@umich.edu',
-      packages=['plato'],
+      packages=['plato', 'plato.prims', 'plato.draw', 'plato.draw.vispy', 'plato.draw.matplotlib'],
       install_requires=['numpy', 'scipy']
       )
