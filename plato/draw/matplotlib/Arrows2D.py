886643f212abb5a3a83486ce61d231b3aa060531
Matthew Spellings <mspells@umich.edu>
2018-03-26 10:51:21 -0400

    Added Arrows2D to matplotlib backend

---
+++ plato/draw/matplotlib/Arrows2D.py
@@ -0,0 +1,9 @@
+import numpy as np
+from ... import draw
+from .Polygons import Polygons
+
+class Arrows2D(draw.Arrows2D, Polygons):
+    __doc__ = draw.Arrows2D.__doc__
+
+    def render(self, *args, **kwargs):
+        Polygons.render(self, *args, **kwargs)
