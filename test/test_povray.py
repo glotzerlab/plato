a552f2114b548c719c17f8f36730deb06fe681d9
Bradley Dice <bdice@bradleydice.com>
2018-03-27 22:03:10 -0400

    Fixed test to use different filenames for each test.

--- test/test_povray.py
+++ test/test_povray.py
@@ -2,19 +2,19 @@ import unittest
 import plato.draw.povray as draw
 import test_scenes
 
-class PovrayTests(unittest.TestCase):
-    NUM_RUN = 0
 
-    def render(self, scene):
-        fname = '/tmp/{}.png'.format(self.NUM_RUN)
-        self.NUM_RUN += 1
+class PovrayTests(unittest.TestCase):
+
+    def render(self, scene, num_run=0):
+        fname = '/tmp/{}.png'.format(num_run)
         scene.save(fname)
 
-for (name, scene) in test_scenes.translate_usable_scenes(draw):
+for i, (name, scene) in enumerate(test_scenes.translate_usable_scenes(draw)):
     new_name = 'test_{}'.format(name)
     setattr(
-        PovrayTests, new_name, (lambda *args, scene=scene, **kwargs:
-                                PovrayTests.render(*args, scene=scene)))
+        PovrayTests, new_name, (lambda *args, scene=scene, num_run=i, **kwargs:
+                                PovrayTests.render(*args, scene=scene,
+                                                   num_run=num_run)))
     getattr(PovrayTests, new_name).__name__ = new_name
 
 if __name__ == '__main__':
