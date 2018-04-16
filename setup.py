ed8dcd73ae47f16b6d7378b6a59268a65a017ef2
Matthew Spellings <mspells@umich.edu>
2018-04-16 13:49:13 -0400

    Update setup.py and tagging v1.0.0

--- setup.py
+++ setup.py
@@ -5,14 +5,19 @@ from setuptools import setup
 with open('plato/version.py') as version_file:
     exec(version_file.read())
 
-setup(name='plato',
-      version=__version__,
-      description='Geometry and visualization tools',
+setup(name='plato-draw',
       author='Matthew Spellings',
       author_email='mspells@umich.edu',
       classifiers=[
-          'License :: OSI Approved :: BSD License'
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python :: 3',
+          'Topic :: Multimedia :: Graphics',
+          'Topic :: Scientific/Engineering :: Visualization'
       ],
+      description='Geometry and visualization tools for collections of particles',
+      install_requires=['numpy', 'scipy'],
+      license='BSD',
       packages=[
           'plato', 'plato.draw',
           'plato.draw.blender',
@@ -20,5 +25,10 @@ setup(name='plato',
           'plato.draw.povray',
           'plato.draw.vispy'
       ],
-      install_requires=['numpy', 'scipy']
+      project_urls={
+          'Documentation': 'http://plato-draw.readthedocs.io/',
+          'Source': 'https://bitbucket.org/glotzer/plato'
+          },
+      python_requires='>=3',
+      version=__version__
       )
