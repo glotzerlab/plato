e415c1b6ada3d49dd640360f6993122a14849802
Bradley Dice <bdice@bradleydice.com>
2018-04-19 22:42:50 -0400

    Fixed JSONShape handling of ndarrays.

--- plato/draw/json/JSONShape.py
+++ plato/draw/json/JSONShape.py
@@ -1,10 +1,13 @@
+import numpy as np
+
 class JSONShape():
 
     def render(self):
         prim_json = {
             'class': self.__class__.__name__,
             'attributes': {
-                k: v.tolist() for k, v in self._attributes.items()},
+                k: v.tolist() if isinstance(v, np.ndarray) else v
+                for k, v in self._attributes.items()},
             'translation': self.translation.tolist(),
             'rotation': self.rotation.tolist()
         }
