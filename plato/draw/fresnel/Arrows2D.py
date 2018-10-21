8b3c36fe5ea261985c19c4d5e1c03e5ca8a2df92
Bradley Dice <bdice@bradleydice.com>
2018-10-21 13:02:33 -0400

    Fixed arrows, but fresnel may not be able to render them.

--- plato/draw/fresnel/Arrows2D.py
+++ plato/draw/fresnel/Arrows2D.py
@@ -1,8 +1,14 @@
+import fresnel
 from ... import draw
 from .Polygons import Polygons
 
 class Arrows2D(draw.Arrows2D, Polygons):
     __doc__ = draw.Arrows2D.__doc__
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, material=None, **kwargs):
+        if material is None:
+            self._material = fresnel.material.Material(primitive_color_mix=1)
+            self._material.solid = 1
+        else:
+            self._material = material
         draw.Arrows2D.__init__(self, *args, **kwargs)
