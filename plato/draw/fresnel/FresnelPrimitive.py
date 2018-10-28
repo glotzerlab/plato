03083ba626ccfc0ee9876da7d193bfcdbe0bfc3f
Bradley Dice <bdice@bradleydice.com>
2018-10-28 15:32:52 -0400

    Manually recreate fresnel defaults.

--- plato/draw/fresnel/FresnelPrimitive.py
+++ plato/draw/fresnel/FresnelPrimitive.py
@@ -3,6 +3,12 @@ import fresnel
 class FresnelPrimitive:
     def __init__(self, *args, material=None, **kwargs):
         if material is None:
-            self._material = fresnel.material.Material(primitive_color_mix=1)
+            self._material = fresnel.material.Material(solid=0,
+                                                       color=(0, 0, 0),
+                                                       primitive_color_mix=1,
+                                                       roughness=0.3,
+                                                       specular=0.5,
+                                                       spec_trans=0,
+                                                       metal=0)
         else:
             self._material = material
