2d1ad3c5528fcc2616dca37bd418f332e6be4c2f
Matthew Spellings <mspells@umich.edu>
2018-11-17 08:34:07 -0500

    Add note about replication of mesh in documentation

--- plato/draw/Mesh.py
+++ plato/draw/Mesh.py
@@ -10,13 +10,17 @@ class Mesh(Shape):
     Meshes are specified by an array of vertices and indices
     identifying triangles within that vertex array. Colors are
     assigned per-vertex and interpolated between vertices.
+
+    Meshes with a common set of vertices and face indices can be
+    replicated multiple times using a set of positions and
+    orientations.
     """
 
     _ATTRIBUTES = list(itertools.starmap(ShapeAttribute, [
         ('vertices', np.float32, (0, 0, 0), 2,
          'Vertex array specifying coordinates of the mesh nodes'),
         ('indices', np.uint32, (0, 0, 0), 2,
-         'Indices of the vertex array specifying individual triangles'),
+         'Indices of the vertex array specifying individual triangles (Nx3)'),
         ('colors', np.float32, (.5, .5, .5, 1), 2,
          'Color, RGBA, [0, 1] for each vertex'),
         ('positions', np.float32, (0, 0, 0), 2,
