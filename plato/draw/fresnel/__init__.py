3a22a6e54a86e89575b11a6599eeefe0a66dbfda
Matthew Spellings <mspells@umich.edu>
2019-01-23 10:57:15 -0500

    Add note about translucency in fresnel backend

--- plato/draw/fresnel/__init__.py
+++ plato/draw/fresnel/__init__.py
@@ -3,6 +3,12 @@ The fresnel backend uses `fresnel <https://bitbucket.org/glotzer/fresnel>`_
 to ray-trace scenes.
 
 All fresnel primitives accept an argument ``material`` of type :py:class:`fresnel.material.Material` to define how lights interact with the primitives.
+
+.. note::
+
+  Translucency is not currently supported in the fresnel backend. All
+  particles will be opaque.
+
 """
 
 from .Scene import Scene
