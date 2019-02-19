a27612760e3d308782155310d9eece504fc0eeee
Bradley Dice <bdice@bradleydice.com>
2019-02-18 23:44:33 -0500

    Add documentation about translucency.

--- plato/draw/pythreejs/__init__.py
+++ plato/draw/pythreejs/__init__.py
@@ -1,3 +1,15 @@
+"""
+The `pythreejs <https://github.com/jupyter-widgets/pythreejs>`_
+backend renders scenes using `three.js <https://threejs.org/>`_
+and is compatible with Jupyter notebooks.
+
+.. note::
+
+    To enable translucency in the pythreejs backend, a primitive must have the
+    same value of alpha (less than 1) for all colors.
+
+"""
+
 from .ConvexPolyhedra import ConvexPolyhedra
 from .ConvexSpheropolyhedra import ConvexSpheropolyhedra
 from .Lines import Lines
