b9180acead098e3a6fd9ecc6203ab1c9b1030815
Matthew Spellings <mspells@umich.edu>
2019-06-27 17:43:18 -0400

    Add pythreejs tests using nbconvert

---
+++ test/test_pythreejs.py
@@ -0,0 +1,17 @@
+import os
+import unittest
+from test_internals import get_fname
+
+from nbconvert.nbconvertapp import NbConvertApp
+
+class PythreejsTests(unittest.TestCase):
+
+    def test_notebook(self):
+        src = os.path.join(os.pardir, 'examples', 'pythreejs test scenes.ipynb')
+        fname = get_fname('pythreejs_test_scenes.html')
+
+        NbConvertApp.launch_instance(
+            argv=['--execute', '--output', fname, src])
+
+if __name__ == '__main__':
+    unittest.main()
